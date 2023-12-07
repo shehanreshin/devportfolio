3cf356edd49872da0efd27ff73a159d6abb57384
shehanreshin <asp.shehanreshin@gmail.com>
2023-12-07 20:53:08 +0530

    refactor: clean up app.js

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,3 +1,4 @@
+// -----------------------------------Local Time Clock-----------------------------------
 function getLocalDate() {
     return new Date(new Date().toLocaleString('en-US', { timeZone: 'Asia/Colombo' }));
 }
@@ -9,14 +10,84 @@ function updateTime() {
     timeOfDay.innerHTML = date.toLocaleTimeString().slice(-2);
 }
 
-
-
 const time = document.getElementById("time");
 const timeOfDay = document.getElementById("time-of-day");
 updateTime();
 
 setInterval(updateTime, 1000);
+// -------------------------------x---Local Time Clock---x-------------------------------
 
+
+// -----------------------------------Smooth Scrolling-----------------------------------
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+    console.log(e)
+})
+
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+// -------------------------------x---Smooth Scrolling---x-------------------------------
+
+
+// -----------------------------------Cursor Animations-----------------------------------
+gsap.registerPlugin(ScrollToPlugin);
+var cursor = document.querySelector('.cursor'),
+    cursorScale = document.querySelectorAll('.cursor-scale'),
+    cursorLink = document.querySelectorAll('.cursor-link'),
+    stylesheet = document.styleSheets[0],
+    statusCircle = document.getElementById('status-circle'),
+    mouseX = 0,
+    mouseY = 0;
+
+gsap.to({}, 0.016, {
+    repeat: -1,
+    onRepeat: () => gsap.set(cursor, {
+        css: {
+            left: mouseX,
+            top: mouseY
+        }
+    })
+}); //update location of cursor
+
+window.addEventListener("mousemove", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+}); //get location of cursor
+
+var iconArrow = cursor.children[0];
+
+cursorScale.forEach(text => {
+    text.addEventListener('mouseleave', () => {
+        cursor.classList.remove('grow');
+        cursor.classList.remove('grow-small');
+    });
+    text.addEventListener('mousemove', () => {
+        cursor.classList.add('grow');
+        if (text.classList.contains('cursor-sm')) {
+            cursor.classList.add('grow-small');
+        }
+    });
+}); //button and nav item hover cursor effect
+
+cursorLink.forEach(container => {
+    container.addEventListener('mouseleave', () => {
+        cursor.classList.remove('grow-link');
+        iconArrow.setAttribute('style', 'opacity: 0;');
+    });
+    container.addEventListener('mousemove', () => {
+        cursor.classList.add('grow-link');
+        iconArrow.setAttribute('style', 'opacity: 1;');
+    });
+}); //socials card hover cursor effect
+// -------------------------------x---Cursor Animations---x-------------------------------
+
+
+// ------------------------------------Event Listeners-----------------------------------
 document.getElementById("socials-linkedin")
     .addEventListener("click", () => window.open("https://www.linkedin.com/in/shehan-reshin"));
 
@@ -55,84 +126,8 @@ document.getElementById('nav-socials')
 
 document.getElementById('nav-back-to-top')
     .addEventListener('click', () => gsap.to(window, { duration: 1.3, scrollTo: 0 }));
+// --------------------------------x---Event Listeners---x-------------------------------
 
 
-gsap.registerPlugin(ScrollToPlugin)
-var cursor = document.querySelector('.cursor'),
-    cursorScale = document.querySelectorAll('.cursor-scale'),
-    cursorLink = document.querySelectorAll('.cursor-link'),
-    stylesheet = document.styleSheets[0],
-    statusCircle = document.getElementById('status-circle'),
-    mouseX = 0,
-    mouseY = 0;
 
-gsap.to({}, 0.016, {
-    repeat: -1,
 
-    onRepeat: () => {
-        gsap.set(cursor, {
-            css: {
-                left: mouseX,
-                top: mouseY
-            }
-        })
-    }
-});
-
-t1 = gsap.timeline();
-
-t1.to({}, 0.016, {
-    repeat: -1,
-
-    onRepeat: () => {
-        gsap.set(statusCircle, {
-            css: {
-
-            }
-        })
-    }
-});
-
-window.addEventListener("mousemove", (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-});
-
-var iconArrow = cursor.children[0];
-
-cursorScale.forEach(text => {
-    text.addEventListener('mouseleave', () => {
-        cursor.classList.remove('grow');
-        cursor.classList.remove('grow-small');
-    });
-    text.addEventListener('mousemove', () => {
-        cursor.classList.add('grow');
-        if (text.classList.contains('cursor-sm')) {
-            cursor.classList.add('grow-small');
-        }
-    });
-});
-
-cursorLink.forEach(container => {
-    container.addEventListener('mouseleave', () => {
-        cursor.classList.remove('grow-link');
-        iconArrow.setAttribute('style', 'opacity: 0;');
-    });
-    container.addEventListener('mousemove', () => {
-        cursor.classList.add('grow-link');
-        iconArrow.setAttribute('style', 'opacity: 1;');
-    });
-});
-
-const lenis = new Lenis()
-
-lenis.on('scroll', (e) => {
-    console.log(e)
-})
-
-function raf(time) {
-    lenis.raf(time)
-    requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
