01db9ceceff59087b0d29d2b0205093d151878bc
shehanreshin <asp.shehanreshin@gmail.com>
2023-12-07 11:00:10 +0530

    feat: add links to the buttons and socials

--- asset/js/app.js
+++ asset/js/app.js
@@ -14,3 +14,27 @@ const timeOfDay = document.getElementById("time-of-day");
 updateTime();
 
 setInterval(updateTime, 1000);
+
+document.getElementById("socials-linkedin")
+    .addEventListener("click", () => window.open("https://www.linkedin.com/in/shehan-reshin"));
+
+document.getElementById("socials-github")
+    .addEventListener("click", () => window.open("https://github.com/shehanreshin"));
+
+document.getElementById("socials-gmail")
+    .addEventListener("click", () => window.open("mailto:asp.shehanreshin@gmail.com"));
+
+document.getElementById("socials-medium")
+    .addEventListener("click", () => window.open("https://medium.com/@shehanreshin"));
+
+document.getElementById("socials-hackerrank")
+    .addEventListener("click", () => window.open("https://www.hackerrank.com/profile/asp_shehanreshin"));
+
+document.getElementById("socials-leetcode")
+    .addEventListener("click", () => window.open("https://leetcode.com/shehanreshin/"));
+
+document.getElementById("socials-frontendmentor")
+    .addEventListener("click", () => window.open("https://www.frontendmentor.io/profile/shehanreshin"));
+
+document.getElementById("socials-codewars")
+    .addEventListener("click", () => window.open("https://www.codewars.com/users/shehanreshin"));
